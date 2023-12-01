e18059882b9f5e3176857ca0b92ddd628c664c52
Showrove Roy <showrove200@gmail.com>
2023-12-01 13:34:58 +0600

    navbar responsive done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,25 @@
 export const Footer = () => {
   return (
     <>
-      {/* <footer className='bg-blue pt-[50px] pb-8'>
+      <footer className='bg-blue pt-[50px] pb-8'>
         <div className='w-maxW1280 flex items-center justify-center'>
           <div className=''>
-            <div className=''></div>
-            <div className="divider"></div> 
+            <div className=''>
+              <div className='flex flex-col w-full lg:flex-row'>
+                <div className='grid flex-grow h-32 card bg-base-300 rounded-box place-items-center'>
+                  content
+                </div>
+                <div className='divider lg:divider-horizontal'>OR</div>
+                <div className='grid flex-grow h-32 card bg-base-300 rounded-box place-items-center'>
+                  content
+                </div>
+                <div className='divider lg:divider-horizontal'>OR</div>
+                <div className='grid flex-grow h-32 card bg-base-300 rounded-box place-items-center'>
+                  content
+                </div>
+              </div>
+            </div>
+            <div className='w-full h-1 bg-white'></div>
             <div className=''>
               <p className='font-semibold text-white'>
                 © 2020-2023 Web Traffic Magnet Digital Ltd. All Rights Reserved.
@@ -13,7 +27,7 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-      </footer> */}
+      </footer>
     </>
   );
 };
