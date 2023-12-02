1e8f9344dda085cc7f46ee319e99ee427b13ca43
Showrove Roy <showrove200@gmail.com>
2023-12-02 13:53:19 +0600

    home slider done

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
 
 export const HeroSection = () => {
   return (
-    <section className='h-screen heroBG'>
+    <section className=' heroBG'>
       <Swiper
         pagination={true}
         autoplay={{
@@ -21,47 +21,105 @@ export const HeroSection = () => {
         }}
         modules={[Pagination, Autoplay]}
         className='mySwiper'>
+        {/* Slider 1 */}
         <SwiperSlide>
-          <div className='hero-content flex-col lg:flex-row-reverse py-10 gap-5'>
-            <div className=' w-1/2'>
-              <img src={`${Laptop}`} className='max-w-xl' />
+          <div className='hero-content flex-col lg:flex-row-reverse md:py-20 py-10 gap-5'>
+            {/* slider right & res-top */}
+            <div className='lg:w-1/2 w-full'>
+              <img src={`${Laptop}`} className='lg:max-w-xl w-full' />
             </div>
-            <div className='w-1/2 '>
-              <h1 className='text-5xl font-bold text-black-10'>
+            {/* slider left & res-bottom */}
+            <div className='lg:w-1/2 w-full '>
+              <h1 className='md:text-5xl text-2xl font-bold text-black-10 lg:text-left text-center'>
                 Unlock Your Online Potential with Web Traffic Magnet
               </h1>
-              <p className='py-6 text-black-10'>
+              <p className='py-2 md:py-5 text-black-10 lg:text-left text-center md:text-base text-xs'>
                 Elevate your brand with expert strategies. Drive web traffic,
                 boost engagement and position your business for success.
               </p>
-              <div className='flex gap-8'>
+              {/* slider CTA button */}
+              <div className='flex sm:flex-row flex-col md:gap-8 gap-2 sm:my-5 my-2 lg:justify-start justify-center items-center '>
+                {/* slider btn 1 */}
                 <Link
                   to=''
-                  className='py-4 px-10 bg-blue rounded-full text-white  btnShadow hover:shadow-none'>
+                  className='py-4 px-10 bg-blue rounded-full text-white  btnShadow hover:shadow-none w-fit text-sm'>
                   Explore Our Services
                 </Link>
+                {/* slider btn 2 */}
                 <Link
                   to=''
-                  className='py-4 px-10 text-blue border border-blue rounded-full hover:btnShadow'>
+                  className='py-4 px-10 text-blue border border-blue rounded-full hover:btnShadow w-fit text-sm'>
                   Contact Us
                 </Link>
               </div>
             </div>
           </div>
         </SwiperSlide>
+        {/* Slider 2 */}
         <SwiperSlide>
-          <div className='hero-content flex-col lg:flex-row-reverse'>
-            <div className=' w-1/2'>
-              <img src={`${Laptop}`} className='max-w-xl' />
+          <div className='hero-content flex-col lg:flex-row-reverse md:py-20 py-10 gap-5'>
+            {/* slider right & res-top */}
+            <div className='lg:w-1/2 w-full'>
+              <img src={`${Laptop}`} className='lg:max-w-xl w-full' />
             </div>
-            <div className='w-1/2'>
-              <h1 className='text-5xl font-bold'>Box Office News!</h1>
-              <p className='py-6'>
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+            {/* slider left & res-bottom */}
+            <div className='lg:w-1/2 w-full '>
+              <h1 className='md:text-5xl text-2xl font-bold text-black-10 lg:text-left text-center'>
+                Unlock Your Online Potential with Web Traffic Magnet
+              </h1>
+              <p className='py-2 md:py-5 text-black-10 lg:text-left text-center md:text-base text-xs'>
+                Elevate your brand with expert strategies. Drive web traffic,
+                boost engagement and position your business for success.
               </p>
-              <button className='btn btn-primary'>Get Started</button>
+              {/* slider CTA button */}
+              <div className='flex sm:flex-row flex-col md:gap-8 gap-2 sm:my-5 my-2 lg:justify-start justify-center items-center '>
+                {/* slider btn 1 */}
+                <Link
+                  to=''
+                  className='py-4 px-10 bg-blue rounded-full text-white  btnShadow hover:shadow-none w-fit text-sm'>
+                  Explore Our Services
+                </Link>
+                {/* slider btn 2 */}
+                <Link
+                  to=''
+                  className='py-4 px-10 text-blue border border-blue rounded-full hover:btnShadow w-fit text-sm'>
+                  Contact Us
+                </Link>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        {/* Slider  3*/}
+        <SwiperSlide>
+          <div className='hero-content flex-col lg:flex-row-reverse md:py-20 py-10 gap-5'>
+            {/* slider right & res-top */}
+            <div className='lg:w-1/2 w-full'>
+              <img src={`${Laptop}`} className='lg:max-w-xl w-full' />
+            </div>
+            {/* slider left & res-bottom */}
+            <div className='lg:w-1/2 w-full '>
+              <h1 className='md:text-5xl text-2xl font-bold text-black-10 lg:text-left text-center'>
+                Unlock Your Online Potential with Web Traffic Magnet
+              </h1>
+              <p className='py-2 md:py-5 text-black-10 lg:text-left text-center md:text-base text-xs'>
+                Elevate your brand with expert strategies. Drive web traffic,
+                boost engagement and position your business for success.
+              </p>
+              {/* slider CTA button */}
+              <div className='flex sm:flex-row flex-col md:gap-8 gap-2 sm:my-5 my-2 lg:justify-start justify-center items-center '>
+                {/* slider btn 1 */}
+                <Link
+                  to=''
+                  className='py-4 px-10 bg-blue rounded-full text-white  btnShadow hover:shadow-none w-fit text-sm'>
+                  Explore Our Services
+                </Link>
+                {/* slider btn 2 */}
+                <Link
+                  to=''
+                  className='py-4 px-10 text-blue border border-blue rounded-full hover:btnShadow w-fit text-sm'>
+                  Contact Us
+                </Link>
+              </div>
             </div>
           </div>
         </SwiperSlide>
