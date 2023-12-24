c3e631fe49290846438bc342c4e5701eb05742c3
Showrove Roy <showrove200@gmail.com>
2023-12-25 00:25:56 +0600

    counter responsive done

--- src/Components/BuildBrand/Countre/Counter/Counter.jsx
+++ src/Components/BuildBrand/Countre/Counter/Counter.jsx
@@ -2,34 +2,33 @@ import CountUp from "react-countup";
 
 export const Counter = () => {
   return (
-    <section className='mt-0'>
-      
-      <div className='brandSectionBG2 md:py-14 py-10 flex flex-col items-center justify-center'>
-        <div className='md:flex gap-10'>
-          <div className='w-[228px] h-[129px] bg-[#fff9f9] rounded'>
-            <p className='text-[#3391FF] text-center  font-[48px] text-[48px]'>
-              <CountUp end={10} />+
+    <section className='mt-0 brandSectionBG2 maxW1280'>
+      <div className='md:py-14 py-10 flex flex-col items-center justify-center'>
+        <div className='flex gap-10 flex-col md:flex-row'>
+          <div className='w-full max-w-xs min-h-[10rem] lg:px-14 px-8 bg-[#fff9f9] rounded-3xl flex flex-col justify-center'>
+            <p className='text-blue text-center lg:text-4xl md:text-2xl text-xl font-semibold'>
+              <CountUp end={5} />+
             </p>
-            <p className='text-center text-2xl'>
-              Years Of{" "}
-              <span className='text-[#3391FF] text-center'>Experience</span>
+            <p className='text-center lg:text-xl md:text-lg text-base font-semibold'>
+              Years Of
+              <span className='text-blue text-center'> Experience</span>
             </p>
           </div>
-          <div className='w-[228px] h-[129px] bg-[#fff9f9] rounded'>
-            <p className='text-[#3391FF] text-center mt-10 text-2xl'>
+          <div className='w-full max-w-xs min-h-[10rem] lg:px-14 px-8 bg-[#fff9f9] rounded-3xl flex flex-col justify-center'>
+            <p className='text-blue text-center lg:text-4xl md:text-2xl text-xl font-semibold'>
               <CountUp end={100} />+
             </p>
-            <p className='text-center text-2xl'>
-              Happy <span className='text-[#3391FF] text-center'>Clients</span>
+            <p className='text-center lg:text-xl md:text-lg text-base font-semibold'>
+              Happy <span className='text-blue text-center'> Clients</span>
             </p>
           </div>
-          <div className='w-[228px] h-[129px] bg-[#fff9f9] rounded'>
-            <p className='text-[#3391FF] text-center mt-10 font-[48px]'>
+          <div className='w-full max-w-xs min-h-[10rem] py-10 lg:px-14 px-8 bg-[#fff9f9] rounded-3xl flex flex-col justify-center'>
+            <p className='text-blue text-center lg:text-4xl md:text-2xl text-xl font-semibold'>
               <CountUp end={24} />|<CountUp end={7} />
             </p>
-            <p className='text-center text-2xl'>
-              Years Of{" "}
-              <span className='text-[#3391FF] text-center'>Online Support</span>
+            <p className='text-center lg:text-xl md:text-lg text-base font-semibold'>
+              Years Of
+              <span className='text-blue text-center'> Online Support</span>
             </p>
           </div>
         </div>
