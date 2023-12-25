a4a868ba17ccafbc242c24f47c4e9bf99a759a46
Showrove Roy <showrove200@gmail.com>
2023-12-26 01:06:15 +0600

    Whats app added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Route/Route";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
+    <FloatingWhatsApp
+            phoneNumber="+1-917-327-2801"
+            accountName="Web Traffic"
+            allowEsc
+            allowClickAway
+            notification
+            notificationSound
+          />
   </React.StrictMode>
 );
