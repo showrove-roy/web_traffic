0f67e18797ac145811bd08b094a71a5aef45a65c
Showrove Roy <showrove200@gmail.com>
2024-01-06 23:52:28 +0600

    card size fixd

--- src/Components/ServiceCard/ServiceCard.jsx
+++ src/Components/ServiceCard/ServiceCard.jsx
@@ -3,21 +3,20 @@ import { BlueButton } from "../BlueButton/BlueButton";
 
 // eslint-disable-next-line react/prop-types
 export const ServiceCard = ({ services }) => {
-  const {id, picture, name, descripton } = services;
+  const { id, picture, name, descripton } = services;
   return (
-    <div className='serviceCardBg rounded-3xl h-[450px] w-[300px]  p-7 '>
+    <div className='serviceCardBg rounded-3xl w-[300px]  p-7 '>
       <div className='flex justify-center'>
         <img src={picture} alt='' />
       </div>
-      <div className=''>
+      <div className='min-h-[250px]'>
         <h4 className='text-3xl text-black-10 font-semibold text-center py-5'>
           {name}
         </h4>
         <p className='text-center text-base text-[#000]'>{descripton}</p>
-
-        <div className='mt-10 flex justify-center '>
-          <BlueButton btnLink={`service/${id}`} btnText={"Get Started"} />
-        </div>
+      </div>
+      <div className='mt-10 flex justify-center '>
+        <BlueButton btnLink={`service/${id}`} btnText={"Get Started"} />
       </div>
     </div>
   );
